0d7f799a12b4e913800fadefea388f33b15815fa
Curtis Dulmage <curtdulmage@gmail.com>
2022-01-18 16:42:14 -0500

    💥 [YouTube] No longer support short url (#22)
    * :boom: [YouTube] No longer support short url
    
    * :memo: [Changeset] Create report

--- src/networks/youtube.ts
+++ src/networks/youtube.ts
@@ -6,11 +6,10 @@ export const youtube: SocialiteNetwork = {
   preferredUrl: `https://youtube.com/channel/${profileReplacement.user}`,
   appUrl: `https://m.youtube.com/c/${profileReplacement.user}`,
   matcher: {
-    // `youtu.be` URLs do not currently support user channels.
-    // We are capturing that anyways... in case this changes in the future.
-    domain: /youtu/,
-    // YouTube has a legacy `/user/*` URL.
-    // We do not support this at the moment... but might reconsider.
-    user: /^(?:\/channel\/|\/c\/)?(?:\/)?([^/]+)/,
+    // TODO: Worth considering if we match against short URLs.
+    // https://github.com/beefchimi/socialite/issues/21
+    domain: /youtube/,
+    // Not currently supporting the legacy `/user/*` URL.
+    user: /^\/(?:channel|c)\/([^/]+)/,
   },
 };
