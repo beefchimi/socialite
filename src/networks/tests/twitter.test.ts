ade619344d9919111c30c506ec21d64748eb8bb6
Curtis Dulmage <curtdulmage@gmail.com>
2022-01-20 17:29:59 -0500

    ✨ [Socialite] Various improvements + bug fixes (#39)
    * :sparkles: [Twitter] Can now match urls without a prefix
    
    * :memo: [Changeset] Create report

--- src/networks/tests/twitter.test.ts
+++ src/networks/tests/twitter.test.ts
@@ -5,9 +5,10 @@ import {twitter} from '../twitter';
 
 describe('Social networks > twitter', () => {
   const mockSocialite = new Socialite(allSocialiteNetworks);
-  const mockCommonUrl = `https://www.twitter.com/${twitter.prefix}${mockGenericUser}`;
 
   it('returns expected `id`, `user`, and `prefix` from common url', () => {
+    const mockCommonUrl = `https://www.twitter.com/${mockGenericUser}`;
+
     const {id, user, prefix} = mockSocialite.parseProfile(
       mockCommonUrl,
     ) as SocialiteProfile;
@@ -17,8 +18,20 @@ describe('Social networks > twitter', () => {
     expect(prefix).toBe(twitter.prefix);
   });
 
+  it('returns expected `id`, `user`, and `prefix` from prefixed url', () => {
+    const mockPrefixedUrl = `https://www.twitter.com/${twitter.prefix}${mockGenericUser}`;
+
+    const {id, user, prefix} = mockSocialite.parseProfile(
+      mockPrefixedUrl,
+    ) as SocialiteProfile;
+
+    expect(id).toBe(twitter.id);
+    expect(user).toBe(mockGenericUser);
+    expect(prefix).toBe(twitter.prefix);
+  });
+
   it('returns expected `id`, `user`, and `prefix` from url with trailing path', () => {
-    const mockUncommonUrl = `${mockCommonUrl}/trail-123`;
+    const mockUncommonUrl = `https://www.twitter.com/${twitter.prefix}${mockGenericUser}/trail-123`;
     const {id, user, prefix} = mockSocialite.parseProfile(
       mockUncommonUrl,
     ) as SocialiteProfile;
