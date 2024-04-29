65b2c1e9e9c87ab018eae7bf47ec1e1c2a35041b
beefchimi <curtdulmage@gmail.com>
2024-04-28 20:23:18 -0400

    :hammer_and_wrench: [Vite] Mark peer deps as external

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import {defineConfig} from 'vite';
 import dts from 'vite-plugin-dts';
 
+import pkg from './package.json';
+
 export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
       name: 'socialite',
       fileName: (format) => `socialite.${format}.js`,
+    },
+    rollupOptions: {
+      // We might need to define global variables
+      // to use in the UMD build.
+      external: Object.keys(pkg.peerDependencies),
     },
     minify: false,
   },
