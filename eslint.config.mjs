87d8d6879a14967da3045d0607c0e6f4cca78a00
beefchimi <curtdulmage@gmail.com>
2024-05-30 14:16:32 -0400

    :hammer_and_wrench: [Eslint] Finish converting new flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,48 @@
-{
-  "env": {
-    "browser": true,
-    "es2022": true
+import globals from 'globals';
+import configLove from 'eslint-config-love';
+// Includes both `config` and `plugin`.
+import pluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ignores: ['coverage/**', 'dist/**'],
   },
-  "extends": ["love", "plugin:prettier/recommended"],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+  configLove,
+  pluginPrettier,
+  {
+    name: 'custom-rules',
+    files: ['**/*.ts', '**/*.js', '**/*.mjs'],
+    languageOptions: {
+      ...configLove.languageOptions,
+      globals: {
+        ...globals.browser,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/strict-boolean-expressions': 'off',
+
+      // This is preferred over `tsconfig` equivalents.
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          ignoreRestSiblings: true,
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+
+      // Consider this rule:
+      /*
+      '@typescript-eslint/consistent-indexed-object-style': [
+        'error',
+        'index-signature',
+      ],
+      */
+    },
   },
-  "rules": {
-    "no-console": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/consistent-indexed-object-style": [
-      "error",
-      "index-signature"
-    ],
-    // This is preferred over `tsconfig` equivalents.
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "ignoreRestSiblings": true,
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_",
-        "caughtErrorsIgnorePattern": "^_"
-      }
-    ],
-    "@typescript-eslint/strict-boolean-expressions": "off"
-  }
-}
+];
